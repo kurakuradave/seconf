b758f4cbd5a52352cf03088009b9acfca51f2a1f
David Effendi <kurakuradave@gmail.com>
2016-11-02 13:23:42 +0800

    added enabling MELPA repository
    added global auto complete

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 ;; libraries and packages
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
@@ -17,7 +17,7 @@
 ( set-foreground-color "Wheat" )
 ( set-cursor-color "Orange" )
 (set-face-attribute 'default nil :font "Monospace-16")
-  
+
 ;; scroll one line at a time (less "jumpy" than defaults)
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
 (setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
@@ -34,3 +34,18 @@
 
 ;;emacspeak
 (setq espeak-default-speech-rate 410)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (auto-complete))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'auto-complete )
+(global-auto-complete-mode t)
